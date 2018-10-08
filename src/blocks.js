254709fc05cb7466f5286e16454eb2d5a4356bb6
Artur Arseniev <artur.catch@hotmail.it>
2018-10-08 16:58:56 +0200

    Add tooltip block

--- src/blocks.js
+++ src/blocks.js
@@ -1,4 +1,14 @@
-export default (editor, config = {}) => {
-  const bm = editor.BlockManager;
-  // ...
+export default (editor, opts = {}) => {
+  const { blockTooltip, blockLabel, id } = opts;
+
+  blockTooltip && editor.BlockManager.add(id, {
+    label: `<svg viewBox="0 0 24 24">
+        <path d="M4 2h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2h-4l-4 4-4-4H4c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2m0 2v12h4.83L12 19.17 15.17 16H20V4H4z"></path>
+      </svg>
+      <div>${blockLabel}</div>`,
+    category: 'Extra',
+    select: true,
+    content: { type: id },
+    ...blockTooltip
+  });
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import loadBlocks from './blocks';
 
 export default (editor, opts = {}) => {
   const options = { ...{
-    // default options
+    // The ID used to create tooltip block and component
+    id: 'tooltip',
+
+    // Label of the tooltip block
+    blockLabel: 'Tooltip',
+
+    // Object to extend the default tooltip block, eg. { label: 'Tooltip', category: 'Extra', ... }.
+    // Pass a falsy value to avoid adding the block
+    blockTooltip: {},
+
+    // Object to extend the default tooltip properties, eg. `{ name: 'Tooltip', droppable: false, ... }`
+    propsTooltip: {},
   },  ...opts };
 
   // Add components
