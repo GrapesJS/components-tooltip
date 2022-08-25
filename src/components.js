4e4d4b0df172f4d58c76b6cd8c1c20437616513f
Artur Arseniev <artur.catch@hotmail.it>
2022-08-25 14:39:53 +0200

    Move styles inside the component

--- src/components.js
+++ src/components.js
@@ -1,5 +1,4 @@
 export default (editor, opts = {}) => {
-  const cssc = editor.CssComposer;
   const {
     id,
     labelTooltip,
@@ -48,127 +47,120 @@ export default (editor, opts = {}) => {
     })
   }
 
-  const createCssStyles = () => {
-    let css = style || `
-      .${classTooltip} {
-        position: relative;
-        display: inline-block;
-        vertical-align: top;
-      }
-
-      .${classTooltipEmpty} {
-        width: 50px;
-        height: 50px;
-      }
-
-      .${classTooltipBody},
-      [${attrTooltip}]::after {
-        font-family: Helvetica, sans-serif;
-        background: rgba(55, 61, 73, 0.95);
-        border-radius: 3px;
-        bottom: 100%;
-        color: #fff;
-        content: attr(${attrTooltip});
-        display: block;
-        font-size: 12px;
-        left: 50%;
-        line-height: normal;
-        max-width: 32rem;
-        opacity: 0;
-        overflow: hidden;
-        padding: 8px 16px;
-        pointer-events: none;
-        position: absolute;
-        text-overflow: ellipsis;
-        transform: translate(-50%, 0);
-        transition: opacity 0.25s, transform 0.25s;
-        white-space: nowrap;
-        box-sizing: border-box;
-        z-index: 10;
-      }
-
-      [${attrTooltipVis}=true]::after,
-      [${attrTooltip}]:focus::after,
-      [${attrTooltip}]:hover::after {
-        opacity: 1;
-        transform: translate(-50%, -0.5rem);
-      }
-
-      [${attrTooltipPos}=right]::after {
-        bottom: 50%;
-        left: 100%;
-        transform: translate(0, 50%);
-      }
-
-      [${attrTooltipPos}=right]:focus::after,
-      [${attrTooltipPos}=right]:hover::after,
-      [${attrTooltipVis}=true][${attrTooltipPos}=right]::after {
-        transform: translate(0.5rem, 50%);
-      }
-
-      [${attrTooltipPos}=bottom]::after {
-        bottom: auto;
-        top: 100%;
-        transform: translate(-50%, 0);
-      }
-
-      [${attrTooltipPos}=bottom]:focus::after,
-      [${attrTooltipPos}=bottom]:hover::after,
-      [${attrTooltipVis}=true][${attrTooltipPos}=bottom]::after {
-        transform: translate(-50%, 0.5rem);
-      }
-
-      [${attrTooltipPos}=left]::after {
-        bottom: 50%;
-        left: auto;
-        right: 100%;
-        transform: translate(0, 50%);
-      }
-
-      [${attrTooltipPos}=left]:focus::after,
-      [${attrTooltipPos}=left]:hover::after,
-      [${attrTooltipVis}=true][${attrTooltipPos}=left]::after {
-        transform: translate(-0.5rem, 50%);
-      }
-
-      [${attrTooltipLen}=small]::after {
-        white-space: normal;
-        width: 80px;
-      }
-
-      [${attrTooltipLen}=medium]::after {
-        white-space: normal;
-        width: 150px;
-      }
-
-      [${attrTooltipLen}=large]::after {
-        white-space: normal;
-        width: 300px;
-      }
-
-      [${attrTooltipLen}=fit]::after {
-        white-space: normal;
-        width: 100%;
-      }
-
-      // IE 11 bugfix
-      button[${attrTooltip}] {
-        overflow: visible;
-      }
-    `;
-    cssc.getAll().add(css + styleAdditional);
-  }
-
   editor.Components.addType(id, {
     isComponent: el => el.hasAttribute?.(attrTooltip),
     model: {
       defaults: {
         name: labelTooltip,
         classes: [classTooltip],
-        attributes: {
-          [attrTooltip]: labelTooltip,
-        },
-        'style-signature': [`[${attrTooltip}`, `.${classTooltip}`],
+        attributes: { [attrTooltip]: labelTooltip },
+        styles: (style || `
+          .${classTooltip} {
+            position: relative;
+            display: inline-block;
+            vertical-align: top;
+          }
+
+          .${classTooltipEmpty} {
+            width: 50px;
+            height: 50px;
+          }
+
+          .${classTooltipBody},
+          [${attrTooltip}]::after {
+            font-family: Helvetica, sans-serif;
+            background: rgba(55, 61, 73, 0.95);
+            border-radius: 3px;
+            bottom: 100%;
+            color: #fff;
+            content: attr(${attrTooltip});
+            display: block;
+            font-size: 12px;
+            left: 50%;
+            line-height: normal;
+            max-width: 32rem;
+            opacity: 0;
+            overflow: hidden;
+            padding: 8px 16px;
+            pointer-events: none;
+            position: absolute;
+            text-overflow: ellipsis;
+            transform: translate(-50%, 0);
+            transition: opacity 0.25s, transform 0.25s;
+            white-space: nowrap;
+            box-sizing: border-box;
+            z-index: 10;
+          }
+
+          [${attrTooltipVis}=true]::after,
+          [${attrTooltip}]:focus::after,
+          [${attrTooltip}]:hover::after {
+            opacity: 1;
+            transform: translate(-50%, -0.5rem);
+          }
+
+          [${attrTooltipPos}=right]::after {
+            bottom: 50%;
+            left: 100%;
+            transform: translate(0, 50%);
+          }
+
+          [${attrTooltipPos}=right]:focus::after,
+          [${attrTooltipPos}=right]:hover::after,
+          [${attrTooltipVis}=true][${attrTooltipPos}=right]::after {
+            transform: translate(0.5rem, 50%);
+          }
+
+          [${attrTooltipPos}=bottom]::after {
+            bottom: auto;
+            top: 100%;
+            transform: translate(-50%, 0);
+          }
+
+          [${attrTooltipPos}=bottom]:focus::after,
+          [${attrTooltipPos}=bottom]:hover::after,
+          [${attrTooltipVis}=true][${attrTooltipPos}=bottom]::after {
+            transform: translate(-50%, 0.5rem);
+          }
+
+          [${attrTooltipPos}=left]::after {
+            bottom: 50%;
+            left: auto;
+            right: 100%;
+            transform: translate(0, 50%);
+          }
+
+          [${attrTooltipPos}=left]:focus::after,
+          [${attrTooltipPos}=left]:hover::after,
+          [${attrTooltipVis}=true][${attrTooltipPos}=left]::after {
+            transform: translate(-0.5rem, 50%);
+          }
+
+          [${attrTooltipLen}=small]::after {
+            white-space: normal;
+            width: 80px;
+          }
+
+          [${attrTooltipLen}=medium]::after {
+            white-space: normal;
+            width: 150px;
+          }
+
+          [${attrTooltipLen}=large]::after {
+            white-space: normal;
+            width: 300px;
+          }
+
+          [${attrTooltipLen}=fit]::after {
+            white-space: normal;
+            width: 100%;
+          }
+
+          // IE 11 bugfix
+          button[${attrTooltip}] {
+            overflow: visible;
+          }
+        `) + styleAdditional,
         traits: extendTraits([
           {
             name: attrTooltip,
@@ -224,11 +216,6 @@ export default (editor, opts = {}) => {
       checkEmpty() {
         const empty = !this.components().length;
         this[empty ? 'addClass' : 'removeClass'](`${classTooltipEmpty}`);
-      }
-    },
-    view: {
-      init() {
-        !cssc.getClassRule(classTooltip) && createCssStyles();
       }
     },
   });
